6e2721db19ccd162678c36c9533dbc80308db3ff
Oleg <olen2006@gmail.com>
2019-07-21 17:33:14 -0600

    added copy,move,delete functions

--- Business Automation with Python/02_ReadWriteFromFile/copymove.py
+++ Business Automation with Python/02_ReadWriteFromFile/copymove.py
@@ -11,13 +11,13 @@ def mergepaths (path1, path2):
     result = path2
     parts1 = tail1.split('\\') if '\\' in tail1 else tail1.split('/')
     parts2 = tail2.split('\\') if '\\' in tail2 else tail2.split('/')
-        for pitem in parts1:
+    for pitem in parts1:
             if pitem != '':
                 if not pitem in parts2:
                     pieces.append(pitem)
-        for piece in pieces:
+    for piece in pieces:
             result = os.path.join(result,piece)
-        return result
+    return result
 
 def fileaction(options):
     if len(options['type']) == 1:
@@ -30,12 +30,12 @@ def cmdfolder(origin,dest,opt):
         #everything fls and sflds found in os.walk origin path is passed to func
         cmdfoldercore(fld,dest,opt,sflds,fnames)
 
-#part1
+
 def cmdfoldercore(fld,dest,opt,sflds,fnames):
     print('processing folder: ' + fld)
     cmdsubfolder(fld,opt,sflds) #part1
     cmdfiles(fld,dest,opt,fnames)
-#part1
+
 def cmdsubfolder(fld, opt,sflds):
     for sf in sflds:
         print('Processing subfolder: ' + sf + ' in ' + fld)
@@ -45,16 +45,50 @@ def cmdfiles(fld,dest,opt,fnames):
         fn = os.path.join(fld,fname)
         if opt == 'c':
             filecopy(fname,fld,dest)
-            elif opt == 'm':
+        elif opt == 'm':
                 filemove(fname,fld,dest)
-            elif opt == 'd':
+        elif opt == 'd':
                 filedelete(fname,fld,dest)
+
 def filecopy(fname,fld,dest):
-    pass
-    #...#
+    fn = os.path.join(fld,fname)
+    d = mergepaths(fld,dest)
+    try:
+        #check destination path and if not, then creat it.
+        if not os.path.exists(d):
+            os.makedirs(d)
+        #copy files from origin to desitnation
+        shutil.copy(fn,d)
+    except IOError, ioerr:
+        print('Error copying file: ' + fname + ' in ' + ' with exception ' + str(ioerr))
+    finally:
+        print('Copied file: ' + fname + ' in ' + fld)
 
+def filemove(fname,fld,dest):
+    fn = os.path.join(fld,fname)
+    d = mergepaths(fld,dest)
+    try:
+        #check destination path and if not, then creat it.
+        if not os.path.exists(d):
+            os.makedirs(d)
+        #copy files from origin to desitnation
+        shutil.move(fn,d)
+    except IOError, ioerr:
+        print('Error moving file: ' + fname + ' in ' + ' with exception ' + str(ioerr))
+    finally:
+        print('Moved file: ' + fname + ' in ' + fld)+
 
+def filedelete(fname,fld,dest):
+    fn = os.path.join(fld,fname)
+    try:
+        os.unlink(fn)
+    except IOError, ioerr:
+        print('Error deleting file: ' + fname + ' in '  + fld)
+    finally:
+        print('Deleted file: ' + fname + ' in ' + fld)
 
+cfg = os.path.splitext(os.path.basename('readwrite'))[0] + '.ini'
+fileaction(readcfg(cfg)[2])
 
 
 
